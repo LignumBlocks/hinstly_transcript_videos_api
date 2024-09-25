abd61f5ee37746e06152fa19ef428fd6dc26fa0b
Roiky Rodríguez Noa <roiky82@gmail.com>
2024-09-25 10:40:01 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+@app.post("/transcribe-videos")
+async def transcribe_videos(urls: list[str]):
+    # Aquí más adelante se colocará la lógica de encolar las tareas
+    return {"message": "Received URLs", "urls": urls}
