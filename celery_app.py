95ccba2b6906b05013c652f3fd09784c95ac5eb4
Roiky Rodríguez Noa <roiky82@gmail.com>
2024-09-28 19:51:14 -0400

    change redis url

--- celery_app.py
+++ celery_app.py
@@ -3,8 +3,10 @@ from celery import Celery
 # Conexión a Redis en localhost (127.0.0.1) en el puerto predeterminado 6379
 celery = Celery(
     "tasks",
-    broker="redis://localhost:6379/0",   # Cambia si tienes Redis en otro puerto
-    backend="redis://localhost:6379/0"
+    #broker="redis://localhost:6379/0", 
+    #backend="redis://localhost:6379/0"
+    broker="redis://red-crs9955ds78s73e2o5og:6379/0", 
+    backend="redis://red-crs9955ds78s73e2o5og:6379/0"
 )
 
 # Importa las tareas
